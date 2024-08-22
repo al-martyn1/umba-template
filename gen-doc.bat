01e07595156b7a4ce2ec31b49bd5172955b8a8cc
Alexandr Martynov <mag@unicont.com>
2024-08-22 15:05:57 +0300

    Дописал про настройку шаблона

--- gen-doc.bat
+++ gen-doc.bat
@@ -1,6 +1,6 @@
 @rem set BATCH_ROOT=--batch-output-path=C:\work\temp\mdpp-test
 @set EXCLUDE_DIRS=--batch-exclude-dir=_libs,libs,_lib,lib,tests,test,rc,_generators,_distr_conf,src,.msvc2019,boost,icons
-@set COMMON=--overwrite --copy-images --add-examples-path=.\src
+@set COMMON=--overwrite --copy-images
 @rem set GRAPHVIZ_OPTIONS=--graphviz-output-path=.\.doc.generated_images
 @rem set TEMP_OPTS=--graphviz-keep-temp-dot-files
 @set OPTIONS= %TEMP_OPTS% %COMMON% %VARS% %EXCLUDE% %GRAPHVIZ_OPTIONS% --batch-scan-recurse=.
