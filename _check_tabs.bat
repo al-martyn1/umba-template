442e91a555f5949ffb5c5c9575982308d4f2e02e
al-martyn1 <amart@mail.ru>
2024-08-24 22:41:48 +0300

    Вынес лишнее в .bat, добавил _precommit.bat

--- _check_tabs.bat
+++
@@ -1,7 +0,0 @@
-@set EMPTY=
-@set PROCESS_FILES_MASKS=*.cpp^,*.c^,*.cc^,*.cxx^,*.c++^,*.hpp^,*.h^,*.hxx^,*.h++^,*.md_^ %EMPTY%
-@set EXCLUDE_FILES_MASKS=*/_libs/*^ %EMPTY%
-@umba-tabtool --check --space --scan --include-files=%PROCESS_FILES_MASKS% --exclude-files=%EXCLUDE_FILES_MASKS% ./doc ./src ./tests
-
-@rem umba-tabtool --help >umba-tabtool.txt
-
---
+++ _precommit.bat
@@ -0,0 +1,1 @@
+@call "%~dp0\.bat\fix_tabs.bat"
