df88bbe692ca26e2dbf63873a6541778d6a6739b
Alexandr Martynov <mag@unicont.com>
2024-09-17 15:49:17 +0300

    Обновил модули и _precommit.bat

--- _precommit.bat
+++ _precommit.bat
@@ -1,2 +1,4 @@
-@call "%~dp0\.bat\fix_tabs.bat"
+@set UMBA_MDPP_EXTRA_OPTIONS="--batch-page-index-file=%~dp0doc\pages_index.md"
+@call "%~dp0.bat\make-cli-options-docs.bat"
+@rem call "%~dp0\.bat\fix_tabs.bat"
 @call "%~dp0\.bat\gen-doc.bat"
