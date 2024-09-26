fb7a71f5a134c6d7afb1072fa296c23e408542c1
Alexandr Martynov <mag@unicont.com>
2024-09-26 17:04:40 +0300

    Обновил модуль .bat и прекоммит скрипт

--- _precommit.bat
+++ _precommit.bat
@@ -1,3 +1,5 @@
+@rem Add next option to UMBA_MDPP_EXTRA_OPTIONS variable to regenerate all images on next generation
+@rem --clear-generation-cache=true
 @set UMBA_MDPP_EXTRA_OPTIONS="--batch-page-index-file=%~dp0doc\pages_index.md"
 @call "%~dp0.bat\make-cli-options-docs.bat"
 @rem call "%~dp0\.bat\fix_tabs.bat"
