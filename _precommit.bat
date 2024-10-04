ee56a0fb8900ebca06b46a476740a8f9b51ff410
Alexandr Martynov <mag@unicont.com>
2024-10-04 13:25:54 +0300

    Добавил в precommit вызов подсчета статистики по сорцам.

--- _precommit.bat
+++ _precommit.bat
@@ -3,4 +3,7 @@
 @set UMBA_MDPP_EXTRA_OPTIONS="--batch-page-index-file=%~dp0doc\pages_index.md"
 @call "%~dp0.bat\make-cli-options-docs.bat"
 @rem call "%~dp0\.bat\fix_tabs.bat"
+@set CLOCMD=md_
+@call "%~dp0\.bat\run_clock.bat"
+@rem 
 @call "%~dp0\.bat\gen-doc.bat"
