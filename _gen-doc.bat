875a2d9c705c695adbc94ac968961a776478484f
al-martyn1 <amart@mail.ru>
2024-11-04 00:12:58 +0300

    Добавил скрипты для отдельной генерации доки

---
+++ _gen-doc.bat
@@ -0,0 +1,2 @@
+@set UMBA_MDPP_EXTRA_OPTIONS="--batch-generate-page-index" "--batch-page-index-file=doc/Index.md"
+@call "%~dp0.bat\gen-doc.bat"
--- _precommit.bat
+++ _precommit.bat
@@ -1,6 +1,6 @@
 @rem Add next option to UMBA_MDPP_EXTRA_OPTIONS variable to regenerate all images on next generation
 @rem --clear-generation-cache=true
-@set UMBA_MDPP_EXTRA_OPTIONS="--batch-page-index-file=%~dp0doc\pages_index.md"
 @call "%~dp0.bat\make-cli-options-docs.bat"
 @rem call "%~dp0\.bat\fix_tabs.bat"
+@set UMBA_MDPP_EXTRA_OPTIONS="--batch-generate-page-index" "--batch-page-index-file=doc/Index.md"
 @call "%~dp0\.bat\gen-doc.bat"
