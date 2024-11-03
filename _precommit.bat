e308b15f2b9c7181a2808451c3e8d8549d6ade19
al-martyn1 <amart@mail.ru>
2024-11-04 00:21:46 +0300

    Вынес генерацию док в отдельный файл и прекоммите только вызов

--- _precommit.bat
+++ _precommit.bat
@@ -2,5 +2,9 @@
 @rem --clear-generation-cache=true
 @call "%~dp0.bat\make-cli-options-docs.bat"
 @rem call "%~dp0\.bat\fix_tabs.bat"
-@set UMBA_MDPP_EXTRA_OPTIONS="--batch-generate-page-index" "--batch-page-index-file=doc/Index.md"
-@call "%~dp0\.bat\gen-doc.bat"
+@if exist "%~dp0.bat\gen-doc.bat" @(
+    @set UMBA_MDPP_EXTRA_OPTIONS="--batch-generate-page-index" "--batch-page-index-file=doc/Index.md"
+    @call "%~dp0.bat\gen-doc.bat"
+) else @(
+    @echo.
+)
