9e2b21f83aab02742bb305ae93e9be615a380bd3
al-martyn1 <amart@mail.ru>
2024-12-08 23:00:53 +0300

    Доопилил батники для запуска тестов

---
+++ tests/_set_config.bat
@@ -0,0 +1,1 @@
+@set CONFIG=Debug
--- tests/_setup_tests.bat
+++ tests/_setup_tests.bat
@@ -1,3 +1,4 @@
 @call "%~dp0\..\.bat\setup_out_root_paths.bat"
 @if exist "%~dp0\..\set_sln.bat" @call "%~dp0\..\set_sln.bat"
-@set SLN_EXE=%OUTROOTPATH%\%SLN%.exe
+@if exist "%~dp0\_set_config.bat" @call "%~dp0\_set_config.bat"
+@set SLN_EXE=%OUTROOTPATH%\%CONFIG%\%SLN%.exe
