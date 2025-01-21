12ac9207a9ddac676d7503ed75747fac14a14481
al-martyn1 <amart@mail.ru>
2025-01-21 23:43:18 +0300

    Обновил скрипты генерации док и прекоммита

--- _precommit.bat
+++ _precommit.bat
@@ -2,11 +2,6 @@
 @rem --clear-generation-cache=true
 @call "%~dp0.bat\make-cli-options-docs.bat"
 
-@set BRIEF_SCAN_PATHS="--scan=%~dp0"
-@rem "--scan=%~dp0\../src" "--scan=%~dp0\../_src" "--scan=%~dp0\../examples"
-@rem set BRIEF_EXTRA_OPTS_TXT=--scan-notes "--notes-output-path=%~dp0\doc\_md"
-@set BRIEF_EXTRA_OPTS_MD=--scan-notes "--notes-output-path=%~dp0\doc\_md"
-
 @rem call "%~dp0\.bat\fix_tabs.bat"
 
 @set CLOCMD=_md
