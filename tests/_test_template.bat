d3f20a80ae341c0b74e3dbc27861d33b73f92918
al-martyn1 <amart@mail.ru>
2025-02-01 20:45:47 +0300

    Добавил в шаблон теста опциональные возможности интерактивного и пакетного запуска

--- tests/_test_template.bat
+++ tests/_test_template.bat
@@ -1,6 +1,16 @@
 @set TEST_NAME=
+
 call "%~dp0\.find_test_exe.bat" %TEST_NAME%
 @echo FOUND_EXE: %FOUND_EXE%
 @set TEST_EXE=%FOUND_EXE%
 
+@if "%1"=="I" goto I
+
 %TEST_EXE%  < "%~dp0\%TEST_NAME%.txt" > "%~dp0\%TEST_NAME%.log" 2>&1
+@goto END
+
+:I
+%TEST_EXE%
+
+:END
+
