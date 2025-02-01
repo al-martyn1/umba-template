bcc873fa83aa07e73bb4c023f3264e6d925aba2a
al-martyn1 <amart@mail.ru>
2025-02-01 19:51:00 +0300

    Обновил скрипты для тестов

---
+++ tests/_test_template.bat
@@ -0,0 +1,6 @@
+@set TEST_NAME=
+call "%~dp0\.find_test_exe.bat" %TEST_NAME%
+@echo FOUND_EXE: %FOUND_EXE%
+@set TEST_EXE=%FOUND_EXE%
+
+%TEST_EXE%
--- tests/test_template.bat
+++
@@ -1,4 +0,0 @@
-@if exist "%~dp0\..\.set_sln.bat" @call "%~dp0\..\.set_sln.bat"
-@call %~dp0\..\.bat\.find-exe-in-out.bat %SLN%
-@echo FOUND_EXE: %FOUND_EXE%
-@set TEST_EXE=%FOUND_EXE%
