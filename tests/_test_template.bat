225a3c465d7901a46a513bce928114e4aa9e05c2
al-martyn1 <amart@mail.ru>
2025-02-28 20:36:43 +0300

    Обновил модуль .bat Подправил шаблон теста

--- tests/_test_template.bat
+++ tests/_test_template.bat
@@ -1,16 +1,12 @@
 @set TEST_NAME=
-
 call "%~dp0\.find_test_exe.bat" %TEST_NAME%
 @echo FOUND_EXE: %FOUND_EXE%
-@set TEST_EXE=%FOUND_EXE%
+@set FOUND_EXE=%FOUND_EXE:"=%
+@if "%FOUND_EXE%"=="" @goto NOT_FOUND
+@set TEST_EXE="%FOUND_EXE%"
+%TEST_EXE% "%~dp0\input.txt"  > %TEST_NAME%-01.log 2>&1
 
-@if "%1"=="I" goto I
 
-%TEST_EXE%  < "%~dp0\%TEST_NAME%.txt" > "%~dp0\%TEST_NAME%.log" 2>&1
-@goto END
-
-:I
-%TEST_EXE%
-
-:END
-
+exit /B 0
+:NOT_FOUND
+@echo Test executable not found
