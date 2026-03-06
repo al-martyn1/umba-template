79d1db975267b4b7c2daf85cb60e289b36b98b5a
al-martyn1 <amart@mail.ru>
2026-03-06 13:26:43 +0300

    Подправил шаблон теста и добавил немного либ в шаблон проекта

--- tests/_test_template.bat
+++ tests/_test_template.bat
@@ -1,10 +1,10 @@
-@set TEST_NAME=
+@set TEST_NAME=%~n0
 call "%~dp0\.find_test_exe.bat" %TEST_NAME%
 @echo FOUND_EXE: %FOUND_EXE%
 @set FOUND_EXE=%FOUND_EXE:"=%
 @if "%FOUND_EXE%"=="" @goto NOT_FOUND
 @set TEST_EXE="%FOUND_EXE%"
-%TEST_EXE% "%~dp0\input.txt"  > %TEST_NAME%-01.log 2>&1
+%TEST_EXE% "%~dp0\%TEST_NAME%.txt"  > %TEST_NAME%-01.log 2>&1
 
 
 exit /B 0
